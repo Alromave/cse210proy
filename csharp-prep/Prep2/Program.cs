25b6c3d3606eac5d7004f6ae9dc1e231f4d221d4
Alromave <122586145+Alromave@users.noreply.github.com>
2023-09-16 14:44:38 -0600

    To practice conditional

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //To practice conditional
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int G = int.Parse(grade);
+        if (G >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (G >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (G >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (G >= 60)
+        {
+            Console.WriteLine("D");
+        }
+
+        else{
+            Console.WriteLine("F");
+        }
+        if (G >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry, better luck next time");
+        }
+
     }
 }
