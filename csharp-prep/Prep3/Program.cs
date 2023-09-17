e34de6c7aca3722470c2491785dc4075aea30e79
Alromave <122586145+Alromave@users.noreply.github.com>
2023-09-17 16:03:15 -0600

    To practice loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        /* parte 1: only conditional
+        Console.Write("What is the magic number? ");
+        string magic = Console.ReadLine();
+        Console.Write("What is the guess number? ");
+        string guess = Console.ReadLine();
+        int M = int.Parse(magic);
+        int G = int.Parse(guess);
+        if (M > G)
+        while (M != G)
+        {
+            Console.Write("Higher");
+        }
+        else if (G>M)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        /*loops
+        //while (G != M)
+        {
+            Console.Write("What is the magic number_");
+            string magic = Console.ReadLine();
+            Console.Write("What is the guess number?");
+            string guess = Console.ReadLine();
+            int M = int.Parse(magic);
+            int G = int.Parse(guess);
+
+        }
+        /*while (M > G)
+        if (G > M)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (G < M)
+        {
+            Console.WriteLine("Lower");
+        }
+        else 
+        {
+            Console.WriteLine("YOu guessed it!");
+        }*/
+        Random randomGenerator = new Random();
+        int M = randomGenerator.Next(1,101);
+        int G = -1;
+        while (G != M)
+        {
+            Console.Write("What is your number guess?");
+            G = int.Parse(Console.ReadLine());
+            if(M > G)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (M < G)
+            {
+                Console.WriteLine("Lower");
+            }
+            else{
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+
+
+
+
+
     }
 }
