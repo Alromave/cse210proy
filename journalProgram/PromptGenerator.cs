ac4d71c39409251e68baaccdc11db131b1e39425
Alromave <122586145+Alromave@users.noreply.github.com>
2023-10-08 11:45:39 -0600

    abstration

--- journalProgram/PromptGenerator.cs
+++ journalProgram/PromptGenerator.cs
@@ -3,9 +3,16 @@ using System.Security.Cryptography.X509Certificates;
 public class PromptGenerator{
     Random random = new Random();
     public List<string> myPrompts = new List<string>(){
-        "1",
-        "2",
-        "3"
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What is the best memory today?",
+        "Some verse from canonical books that struck me today?",
+        
+
+        
     };
     public string getRandomPrompt(){
 
